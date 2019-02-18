56c2970cce6d5003c2a3c597f828df2bc0f4b7a3
Steve SangHee <steve.sanghee@gmail.com>
2019-02-18 16:20:25 -0500

    DartGame

---
+++ src/DartGame/MainApp.java
@@ -0,0 +1,15 @@
+package DartGame;
+
+import java.util.Scanner;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Dart score : ");
+		String inputVal = scanner.nextLine();
+		
+
+	}
+
+}
