60114d02d10786497792950e030f560a3827ca2f
Steve SangHee <steve.sanghee@gmail.com>
2019-02-23 20:52:28 -0500

    nQueens

---
+++ src/nQueens/MainApp.java
@@ -0,0 +1,45 @@
+package nQueens;
+
+public class MainApp {
+	private static int N = 8;
+	private static int[] cols = new int [N+1];
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		queens(0);
+
+	}
+	
+	public static boolean queens(int level) {
+		if (!promising(level)) {
+			return false;
+		}
+		else if (level == N) {
+			for (int i = 1; i<=N; i++) {
+				System.out.println("(" + i + ", " + cols[i] +")");
+			}
+			return true;
+		}
+		
+		for (int i = 1; i <= N; i ++ ) {
+			cols[level+1] = i;
+			if (queens(level+1)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public static boolean promising(int level) {
+		for (int i=1; i < level ; i++) {
+			if (cols[i] == cols [level]) {
+				return false;
+			}
+			else if ((level - i) == Math.abs(cols[level] - cols[i])) {
+				return false;
+			}
+			
+		}
+		return true;
+	}
+
+}
